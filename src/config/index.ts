f854c6aa427efadbffc1a336efa73b487caa8c1b
md-anamul-haque <m.anamul.dev@gmail.com>
2023-06-03 13:01:35 +0600

    change

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 import auth from './auth';
-import db from './db';
+import { connection } from './db';
 dotenv.config();
 const PORT = process.env.PORT || 3302;
 const app_config = {
   PORT,
-  db,
+  connection,
   auth
 }
 
